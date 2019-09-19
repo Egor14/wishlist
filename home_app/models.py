40e639600a7bb6139ed4cea5c933162ce842bcea
Egor14 <egorgrekhnev@mail.ru>
2019-09-19 10:58:22 +0300

    max_lengt

--- home_app/models.py
+++ home_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Wish(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    link = models.CharField(max_length=200)
+    link = models.CharField(max_length=400)
     comment = models.TextField()
 
     def __str__(self):
