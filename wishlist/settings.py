8650fd9243746e15a355a7d24b96c60b08637968
Egor14 <egorgrekhnev@mail.ru>
2019-09-19 10:39:57 +0300

    deploy

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b8)qnvq9=(0ty3r#xbvp4jwx!c*3^zcpjz*5dri$l(ha=b&t83'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wishlist.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,4 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
